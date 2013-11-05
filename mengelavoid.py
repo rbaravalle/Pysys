720b390262ca27fee08eee8cffd74c16b36c92ec
rbaravalle <rbaravalle@gmail.com>
2013-11-05 18:40:08 -0300

    avoid problems

--- mengelavoid.py
+++ mengelavoid.py
@@ -20,10 +20,10 @@ draw = ImageDraw.Draw(I)
 #field = np.zeros((maxX, maxY)).astype(np.uint8) + np.uint8(255)
 
 r = 28 # radius of initial bubbles
-c = 6 # amount of initial bubbles
+c = 20 # amount of initial bubbles
 orig = c
 
-numIt = 1
+numIt = 3
 h = 0
 h2 = 0
 cinf = 4
@@ -31,7 +31,6 @@ cinf = 4
 maxa = -10
 mina = 100000
 
-points2 = np.zeros((c*2)).astype(np.uint32)
 points3 = np.zeros((c*cinf)).astype(np.float32)
 pointstot = points3
 
@@ -53,8 +52,8 @@ def avoid(i,j,rac):
        d = np.sqrt(d)
        rr = pointstot[pos+2]
 
-       print i,j,d
-       print pointstot[pos], pointstot[pos+1], rr,rac
+       #print i,j,d
+       #print pointstot[pos], pointstot[pos+1], rr,rac
        if(np.float32(d)<np.float32(rr)+rac): 
             return False
 
@@ -67,27 +66,21 @@ for k in range(c):
     i = randint(2*r,maxX-r)
     j = randint(2*r,maxY-r)
     if(avoid(i,j,r)):
-        points2[h] = i
-        h = h+1
-        points2[h] = j
-        h = h+1
-        points3[h2] = i
-        points3[h2+1] = j
-        points3[h2+2] = r
-        points3[h2+3] = detcuad(i,j)
-        pointstot = points3
-
-pointstot = points3
+        pointstot[h2] = i
+        pointstot[h2+1] = j
+        pointstot[h2+2] = r
+        pointstot[h2+3] = detcuad(i,j)
+        h2+=cinf
 
 for i in range(numIt):
     print "It num", i
-    print "Total: ", 2*c
-    for h in range(0,2*c,2):
-        x = points2[h]
-        y = points2[h+1]
+    print "Total: ", cinf*c
+    for h in range(0,cinf*c,cinf):
+        x = pointstot[h]
+        y = pointstot[h+1]
         if(h%600 == 0): print h
 
-        r2 = r
+        r2 = r-4
         draw.ellipse((x-r2, y-r2, x+r2, y+r2), fill=(np.uint8(255)))
 
 
@@ -97,27 +90,21 @@ for i in range(numIt):
     c = int(c*cuant)
 
     # reset points
-    points = points2
-    points2 = np.zeros((c*2)).astype(np.uint32)
     points3 = np.zeros((c*cinf)).astype(np.float32)
     pos = 0
-    pos2 = 0
-    for k in range(0,2*orig,2):
+    for k in range(0,cinf*orig,cinf):
         for l in range(cuant):
             d = random.random()*np.pi*2
             rr = 8*r
-            i = points[k]+np.int32(rr*np.cos(d))#+randint(0,10)
-            j = points[k+1]+np.int32(rr*np.sin(d))#+randint(0,10)
-            if(avoid(i,j,r)):
-                points2[pos] = i
-                points2[pos+1] = j
-                points3[pos2] = i
-                points3[pos2+1] = j
-                points3[pos2+2] = r
+            i = pointstot[k]+np.int32(rr*np.cos(d))#+randint(0,10)
+            j = pointstot[k+1]+np.int32(rr*np.sin(d))#+randint(0,10)
+            if(avoid(i,j,r)==True):
+                points3[pos] = i
+                points3[pos+1] = j
+                points3[pos+2] = r
                 cuad = detcuad(i,j)
-                points3[pos2+3] = cuad
-                pos+=2
-                pos2+=cinf
+                points3[pos+3] = cuad
+                pos+=cinf
 
     pointstot = np.hstack((pointstot,points3))
 
