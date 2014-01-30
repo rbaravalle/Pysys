e20c99f078e7faeae45c77956ee0d05b0462cdfd
rbaravalle <rbaravalle@gmail.com>
2014-01-30 17:05:31 -0300

    boundary conditions

--- mass.py
+++ mass.py
@@ -30,7 +30,14 @@ dx=0.004        # Interval size in x-direction.
 dy=0.004        # Interval size in y-direction.
 #a=4          # Diffusion constant.
 #tf = 0.0013
-tf = 100
+
+
+def toKelvin(c):
+    return c + 273.15
+
+
+tOven = toKelvin(200) # temperature Oven
+tf = toKelvin(100)
 timesteps=3000  # Number of time-steps to evolve system.
 
 nx = int(1/dx)
@@ -39,7 +46,9 @@ ny = int(1/dy)
 dx2=dx**2 # To save CPU cycles, we'll compute Delta x^2
 dy2=dy**2 # and Delta y^2 only once and store them.
 a = 4
-tinit = 20#*10**-3#0.006
+
+
+tinit = toKelvin(27) # Zhang&Datta
 tfactor = 1#10**2
 
 
@@ -48,13 +57,18 @@ tfactor = 1#10**2
 dt = dx2*dy2/( 2*a*(dx2+dy2) )#*16
 Dt = 0.5
 
-dt = 0.1
+dt = 0.5
 # Start u and ui off as zero matrices:
-wi = np.array(lsystem.lin()).astype(np.float32) # (255)? cuanto es el valor original de la masa?
-wi = np.max(wi)-wi
+wi = np.array(lsystem.lin()).astype(np.float32)/510  # (255)? cuanto es el valor original de la masa?
+
+wi = np.max(wi)-wi + 0.1
+
+print "MAX W: ",np.max(wi)
+print "MIN W: ", np.min(wi)
+
 wi.flags.writeable = True
-print wi.sum()
-print '{0:.16f}'.format(np.float64(wi.sum()))
+#print wi.sum()
+print 'Total moisture content (W): {0:.16f}'.format(np.float64(wi.sum()))
 
 ti = np.zeros((nx,ny)).astype(np.float32)+tinit
 #for i in range(nx):
@@ -77,16 +91,22 @@ if(False):
 w = np.zeros(wi.shape).astype(np.float64)
 t = np.zeros(ti.shape).astype(np.float32)
 
-# Now, set the initial conditions (ui).
-#for i in range(nx):
-#    for j in range(ny):
-#        if ( ( (i*dx-0.5)**2+(j*dy-0.5)**2 <= 0.1)
-#            & ((i*dx-0.5)**2+(j*dy-0.5)**2>=.05) ):
-#                ui[i,j] = 1
+# forced convection REVISAR
+for i in range(0,1):
+    for j in range(ny-1):
+        ti[i][j] = tOven
 
-#for i in range(10,30):
-    #for j in range(10,30):
-        #ui[i,j] = 1
+for i in range(1,nx-1):
+    for j in range(0,1):
+        ti[i][j] = tOven
+
+for i in range(nx-2,nx-1):
+    for j in range(1,ny-1):
+        ti[i][j] = tOven
+
+for i in range(1,nx-2):
+    for j in range(ny-2,ny-1):
+        ti[i][j] = tOven
 
 # EQUATIONS----------------
 
@@ -156,9 +176,9 @@ def cp(x,y):
     cpw = (5.207 - 73.17 * (10**-4)*ti[x][y] + 1.35*(10**-5)*(ti[x][y]**2))*1000
     cpp = cps + wi[x][y]*cpw
 
-    lam = 2.3 #  unidad ?
+    lam = 2.3 #  ? REVISAR
     nab = 0
-    if(ti[x][y] >= tf): nab = 1
+    if(ti[x][y] >= tf): nab = 1 # REVISAR
     #nab = exp(-((ti[x][y]-tf)**2)/(2*Dt**2)) #1 # ?
     #print "Tf: ", tf
 
@@ -172,29 +192,43 @@ def k(x,y):
 
 ######
 
-Tinf = 20 # ambient REVISAR
-Ts = 60 # ???? BUSCAR
+Tinf = toKelvin(20) # ambient REVISAR
+Ts = toKelvin(60) # ???? BUSCAR
 eps = 0.85 # emissivity of bread surface REVISAR
 sigm = 5.67 * 10**-8 # stefan-boltzmann constant
 ros = 241.76 # solid density
+RH = 0.6 # ??? BUSCAR
+
+# Change as necessary..
+# Forced convection 200 C baking temperature
+hh =11.96
+kg =8.46*(10**-9)
 
 def aw(x,y): # water activity
-    return ((100*w[x][y]/exp(-0.0056*t[x][y]+5.5))**(-1/0.38)+1)**(-1)
-
-def h(x): # ??? BUSCAR
-    return 6
+    v = ((100*wi[x][y]/np.exp(-0.0056*ti[x][y]+5.5)))
+    print "V:", v, x, y
+    if isnan(v):
+        print "T,W,V", ti[x][y], wi[x][y], v, x, y
+        exit()
+    print "AW1: ", np.exp(-0.0056*ti[x][y]+5.5)
+    print "AW3: ", ((100*wi[x][y]/np.exp(-0.0056*ti[x][y]+5.5)))
+    return ((100*wi[x][y]/np.exp(-0.0056*ti[x][y]+5.5))**(-1/0.38) + 1)**(-1)
 
 def rightTBC(x,y):
-    return h(Ts-Tinf)  + eps*sigm*(Ts**4-Tinf**4)
+    return hh*(Ts-Tinf)  + eps*sigm*(Ts**4-Tinf**4)
 
-def Psat(x): # ??? Presion de vapor de agua de saturacion REVISAR FUNCION
-    a = [6.7, 17.2, 23.7, 26.4, 28.6, 32.6, 40, 45.5, 53.6, 59.7, 64.6, 68.7, 75.4, 85.4, 93.0, 99.1,110.8, 119.6, 126.8, 132.9, 138.2, 142.9, 147.2, 151.1, 158.1, 164.2, 169.6, 174.5, 179, 183.2, 187.1,190.7]
-    b = [0.01,0.02,0.03,0.035,0.04,0.05,0.075,0.1,0.15,0.2,0.25,0.3,0.4,0.6,0.8,1,1.5,2,2.5,3, 3.5,4,4.6,5,6,7,8,9,10,11,12,13]
+def Psat(x): # Presion de vapor de agua de saturacion
+    a = [0, 5, 10, 11,12,13,14,15,20,25,30,37,40,60,80,95,96,97,98,99,100,101,110,120,200] # T (C)
+
+    a = map(lambda i:toKelvin(i), a)
+
+    b = [4.58, 6.54, 9.21, 9.84, 10.52, 11.23, 11.99, 12.79, 17.54, 23.76, 31.8, 47.07, 55.3, 149.4, 355.1, 634, 658, 682, 707, 733, 760, 788, 1074.6, 1489, 11659] # P (mmHg)
+
 
     z = np.polyfit(a, b, 3)
     f = np.poly1d(z)
 
-    return f(x)
+    return f(x)*133.3224     # 1 mmHg = 133.3224 Pa (Torr-Pascales)
 
 def Ps(x,y):
     return aw(x,y)*Psat(Ts)
@@ -203,7 +237,7 @@ def Pinf(): # RH: relacion entre la presion de vapor y la presion de vapor de sa
     return RH*Psat(Tinf)
 
 def rightWBC(x,y):
-    return kg*(Ps*Ts-Pinf*Tinf) # REVISAR si es Ps(Ts) o Ps*Ts idem Pinf Tinf
+    return kg*(Ps(x,y)*Ts-Pinf()*Tinf) # REVISAR si es Ps(Ts) o Ps*Ts idem Pinf Tinf
 
 def evolve_ts(w, wi,t,ti):
     """
@@ -213,8 +247,9 @@ def evolve_ts(w, wi,t,ti):
     """
     #u[1:-1, 1:-1] = ui[1:-1, 1:-1] + a*dt*( (ui[2:, 1:-1] - 2*ui[1:-1, 1:-1] + ui[:-2, 1:-1])/dx2 + (ui[1:-1, 2:] - 2*ui[1:-1, 1:-1] + ui[1:-1, :-2])/dy2 )
 
-    for x in range(2,nx-2):
-        for y in range(2,ny-2):
+    p = 0
+    for x in range(2,nx-3):
+        for y in range(2,ny-3):
             h = ddx(x,y)+ddy(x,y)
             v = (ddx(x,y) + ddy(x,y))*dt
             #if(v > 0): print "DDX: ", wi[x][y] + v
@@ -228,19 +263,23 @@ def evolve_ts(w, wi,t,ti):
                 print "Wi: ", ti[x][y]
                 print "Wi2: ", ti[x][y] + np.float64(v2*10**4)
             #print ro(x,y),cp(x,y), v,v2
+
             w[x][y] = wi[x][y] + v
             t[x][y] = ti[x][y] + np.float64(v2)#*10**2)
+            #if(wi[x][y] > 0 and v!= 0 and p== 0): 
+            #    print "x, y, W: ", x, y, w[x][y]
+            #    p = 1
     
 
-    if(False):
+    if(True):
         # Boundary conditions
         for x in range(1,2):
-            for y in range(ny):
+            for y in range(1,ny-2):
                 t[x][y] = (t[x+1][y] + t[x][y+1] + rightTBC(x,y)*dx/k(x,y))/2.0
                 w[x][y] = (w[x+1][y] + w[x][y+1] + rightWBC(x,y)*dx/(D(x,y)*ros))/2.0
 
-        for x in range(nx-2,nx-1):
-            for y in range(ny):
+        for x in range(nx-3,nx-2):
+            for y in range(2,ny-3):
                 t[x][y] = (t[x+1][y] + t[x][y+1] + rightTBC(x,y)*dx/k(x,y))/2.0
                 w[x][y] = (w[x+1][y] + w[x][y+1] + rightWBC(x,y)*dx/(D(x,y)*ros))/2.0
 
@@ -250,10 +289,13 @@ def evolve_ts(w, wi,t,ti):
                 w[x][y] = (w[x+1][y] + w[x][y+1] + rightWBC(x,y)*dx/(D(x,y)*ros))/2.0
 
         for x in range(2,nx-2):
-            for y in range(ny-2,ny-1):
+            for y in range(ny-3,ny-2):
                 t[x][y] = (t[x+1][y] + t[x][y+1] + rightTBC(x,y)*dx/k(x,y))/2.0
                 w[x][y] = (w[x+1][y] + w[x][y+1] + rightWBC(x,y)*dx/(D(x,y)*ros))/2.0
                 
+
+    print "WS:"
+    print wi[1][0:10]
 
 
 def updatefig(*args):
@@ -274,7 +316,7 @@ def updatefig(*args):
 
     #print 255*np.asarray(ui).astype(np.uint8)
     #print wi.sum()
-    print '{0:.16f}'.format(np.float64(wi.sum()))
+    print 'Total moisture content: {0:.16f}'.format(np.float64(wi.sum()))
     print ti.sum()
 
     #I = Image.frombuffer('L',(nx,ny),  255*(255*np.asarray(wi) > 170).astype(np.uint8) ,'raw','L',0,1)
@@ -282,8 +324,11 @@ def updatefig(*args):
     #I = Image.frombuffer('L',(nx,ny),  (255*np.asarray(wi)).astype(np.uint8) ,'raw','L',0,1)
     #I.save('images/image'+str(m)+'.png')
 
-    I = Image.frombuffer('L',(nx,ny),  (255*np.asarray(wi)).astype(np.uint8)+150 ,'raw','L',0,1)
+    print ((np.asarray(wi))).astype(np.uint8)
+
+    I = Image.frombuffer('L',(nx,ny),  (np.asarray(wi)).astype(np.uint8) ,'raw','L',0,1)
     I.save('images/image'+str(m)+'.png')
+    print wi[100][200]
 
     I2 = Image.frombuffer('L',(nx,ny),  (tfactor*255*np.asarray(ti)).astype(np.uint8) ,'raw','L',0,1)
     I2.save('images/timage'+str(m)+'.png')
