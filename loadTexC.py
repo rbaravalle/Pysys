e8ad416feca574f2682884103f268b90f4e8b287
rbaravalle <rbaravalle@gmail.com>
2014-09-21 10:04:53 -0300

    spherical structuring element

--- loadTexC.py
+++ loadTexC.py
@@ -5,17 +5,25 @@ import cprint, time
 import sys 
 import scipy.ndimage
 
+def struct(r):
+    s = np.zeros((r,r,r))
+    for i in range(r):
+        for j in range(r):
+            for k in range(r):
+                if(np.sqrt((i-r/2)**2+(j-r/2)**2+(k-r/2)**2) < r):
+                    s[i,j,k] = 1
+
 def border(data):
     r = 15
     r2 = 10
     r3 = 7
     r4 = 6
-    b1 = scipy.ndimage.binary_closing(data,structure=np.ones((r,r,r))).astype(np.uint8)
-    b1 = scipy.ndimage.binary_dilation(b1,structure=np.ones((r2,r2,r2))).astype(np.uint8)
-    b = scipy.ndimage.binary_erosion(b1,structure=np.ones((r3,r3,r3))).astype(np.uint8)
+    b1 = scipy.ndimage.binary_closing(data,structure=struct(r)).astype(np.uint8)
+    b1 = scipy.ndimage.binary_dilation(b1,structure=struct(r2)).astype(np.uint8)
+    b = scipy.ndimage.binary_erosion(b1,structure=struct(r3)).astype(np.uint8)
     #c = b1-b
-    c = scipy.ndimage.binary_closing(b1-b,structure=np.ones((r4,r4,r4))).astype(np.uint8)
-    c = scipy.ndimage.binary_dilation(c,structure=np.ones((r4,r4,r4))).astype(np.uint8)
+    c = scipy.ndimage.binary_closing(b1-b,structure=struct(r4)).astype(np.uint8)
+    c = scipy.ndimage.binary_dilation(c,structure=struct(r4)).astype(np.uint8)
     return np.array(255*(c)).astype(np.uint8)
 
 I = Image.open(sys.argv[1])
